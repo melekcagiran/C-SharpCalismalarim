f6a0024c57d06ce1541a9959cecb80653d17cb72
melekcagiran <melekcagiran028@gmail.com>
2025-04-13 14:18:12 +0300

    Add files via upload

---
+++ C-SharpCalisması1.cs
@@ -0,0 +1,27 @@
+﻿namespace deneme6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("bir kelime girin:");
+            string kelime=Console.ReadLine();
+            bool sonuc= PalindromMu (kelime);
+            if ( sonuc )
+            {
+                Console.WriteLine($"{kelime} bir palindromdur.");
+            }
+            else
+            {
+                Console.WriteLine($"{kelime} bir palindrom değildir.");
+            }
+            static bool PalindromMu  ( string kelime)
+            {
+                char[] chars = kelime.ToCharArray();
+                Array.Reverse( chars );
+                string terstenKelime=new string( chars );
+                return kelime.Equals( terstenKelime,StringComparison.OrdinalIgnoreCase ) ;
+            }
+        }
+    }
+}
